8aa8d01bad461c831e6818e4203b1f64c1f2832d
oyama919 <oyama919@gmail.com>
2017-06-14 08:56:56 +0900

    質問機能についてコントローラー定義

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,22 +1,50 @@
 class QuestionsController < ApplicationController
-  def index
-  end
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
 
-  def show
+  def index
+      @question = Question.all
   end
 
   def new
+    if params[:back]
+      @question = Question.new(questions_params)
+    else
+      @question = Question.new
+    end
   end
 
   def create
+    @question = Question.new(questions_params)
+    @question.user_id = current_user.id
+    if @question.save
+      redirect_to root_path, notice:"投稿されました"
+    else
+      redirect_to root_path, alert:"未入力の項目があります"
+      @question = Question.new(questions_params)
+    end
   end
 
   def edit
   end
 
   def update
+    @question.update(questions_params)
+    redirect_to questions_path, notice:"投稿を更新しました"
   end
 
   def destroy
+    @question.destroy
+    redirect_to root_path, alert:"投稿を削除しました"
   end
+
+  def show
+  end
+
+  private
+    def questions_params
+      params.require(:question).permit(:title,:content)
+    end
+    def set_question
+      @question = Question.find(params[:id])
+    end
 end
