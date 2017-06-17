329d48c8bbc71cee86648fb5cffb2581eaa62d36
Yusuke Konishi <ysk.kns@gmail.com>
2017-06-17 16:28:19 +0900

    set 0 to vote_count by default

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170617041940) do
+ActiveRecord::Schema.define(version: 20170617071605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,9 +27,9 @@ ActiveRecord::Schema.define(version: 20170617041940) do
     t.string   "title"
     t.string   "content"
     t.integer  "user_id"
-    t.integer  "vote_count"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "vote_count", default: 0
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "tags", force: :cascade do |t|
