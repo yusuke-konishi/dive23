37395009b84f5e44fe8b2451a561b08f7ba11c99
Yusuke Konishi <ysk.kns@gmail.com>
2017-06-17 14:39:51 +0900

    fix #46: add rails_admin for all users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'top#index'
   resources 'questions'
   resources 'users', only: [:index, :show]
