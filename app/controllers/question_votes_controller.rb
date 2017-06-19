2e1a9423202249ab20798b243b00d91dfa1f5e15
Yusuke Konishi <ysk.kns@gmail.com>
2017-06-20 02:24:31 +0900

    modify plus action by vote_state

--- app/controllers/question_votes_controller.rb
+++ app/controllers/question_votes_controller.rb
@@ -1,15 +1,45 @@
 class QuestionVotesController < ApplicationController
+  before_action :check_question_vote
+
   def plus
-    @question_vote = current_user.question_votes.build(question_id: params[:question_id])
-    @question_vote.vote_state = 1
-    @question_vote.save
+    if @question_vote.blank?
+      @question_vote = current_user.question_votes.build(question_id: params[:question_id])
+      @question_vote.vote_state = 1
+      @question_vote.save
 
-    @question_vote.question.vote_count = @question_vote.question.vote_count + 1
-    @question_vote.question.save
+      @question.vote_count = @question.vote_count + 1
+      @question.save
 
-    redirect_to question_path(@question_vote.question), notice: "#{@question_vote.question.user.name} さんの質問に +1 投票しました"
+      redirect_to question_path(@question), notice: "質問に +1 投票しました"
+    else
+      if @question_vote.vote_state == 0
+        @question_vote.vote_state = 1
+        @question_vote.save
+
+        @question.vote_count = @question.vote_count + 1
+        @question.save
+
+        redirect_to question_path(@question), notice: "質問に +1 投票しました"
+      elsif @question_vote.vote_state == -1
+        @question_vote.vote_state = 0
+        @question_vote.save
+
+        @question.vote_count = @question.vote_count + 1
+        @question.save
+
+        redirect_to question_path(@question), notice: "投票をリセットしました"
+      else # @question_vote.vote_state == 1
+        redirect_to question_path(@question), notice: "同じ質問に 2 回以上 +1 投票することはできません"
+      end
+    end
   end
 
   def minus
   end
+
+  private
+    def check_question_vote
+      @question = Question.find(params[:question_id])
+      @question_vote = @question.question_votes.find_by(user_id: current_user.id)
+    end
 end
