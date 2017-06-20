6b795f2bf224dd5541c2ba8a62ca1294dfe980cb
Yusuke Konishi <ysk.kns@gmail.com>
2017-06-20 23:30:27 +0900

    add seed data for questions/answers

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,37 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-3.times do |m|
-  name = "review0#{m+1} user"
-  email = "review0#{m+1}@dive23.com"
+3.times do |i|
+  name = "review0#{i+1} user"
+  email = "review0#{i+1}@dive23.com"
   password = "password"
-  uid = "review0#{m+1}"
+  uid = "review0#{i+1}"
 
   User.create!(name: name,
                email: email,
                password: password,
                password_confirmation: password,
                uid: uid,
+               confirmed_at: Time.now
                )
 
-  user = User.find(m+1)
-  user.update(confirmed_at: Time.now)
+  i = i + 1
+end
 
-  m = m + 1
+3.times do |j|
+  Question.create!(user_id: j+1,
+               title: "review0#{j+1} user と申しますが…",
+               content: "Rails がさっぱりわかりません。この先どうすれば…？"
+               )
+
+    3.times do |k|
+      Answer.create!(question_id: j+1,
+                  user_id: k+1,
+                  content: "review0#{k+1} user と申しますが、どうにもなりません。"
+                  )
+
+      k = k + 1
+    end
+
+  j = j + 1
 end
