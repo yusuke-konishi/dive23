1086fd0bdbbe1970ca5da285b1512fe791e7e6be
越智 昭文 <akifumi.ochi@gmail.com>
2017-06-20 18:38:02 +0900

    お気に入り機能実装

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,18 +1,28 @@
 class BookmarksController < ApplicationController
-  before_action :set_question
 
   def create
-    @bookmark = current_user.bookmarks.build(question_id: @question.id)
-    @question = @bookmark.question
+    @question = Question.find(params[:bookmark][:question_id])
+    @bookmark = Bookmark.new(user_id: current_user.id, question_id: @question.id)
+    respond_to do |format|
+      if @bookmark.save
+         @question.reload
+         format.js
+      else
+         format.html { redirect_to question_path(@question) }
+      end
+    end
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:id])
-    @bookmark.destroy
-  end
-
-  private
-    def set_question
-      @question = Question.find(params[:bookmark][:question_id])
+    @question = Question.find(params[:bookmark][:question_id])
+    @bookmark = Bookmark.find_by(user_id: current_user.id, question_id: @question.id)
+    respond_to do |format|
+      if @bookmark.destroy
+         @question.reload
+         format.js
+      else
+         format.html { redirect_to question_path(@question) }
+      end
     end
+  end
 end
