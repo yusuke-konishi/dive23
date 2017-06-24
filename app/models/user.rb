506f8e574a0a2c00dcf4d092249ec6ce9a4ad86a
越智 昭文 <akifumi.ochi@gmail.com>
2017-06-24 19:52:09 +0900

    googleログインのための設定変更(user.rbやdevise.rb.omniauth_callbacks_controller）

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,23 @@ class User < ActiveRecord::Base
     user
   end
 
+  def self.find_for_google_oauth(auth, signed_in_resource=nil)
+    user = User.find_by(email: auth.info.email)
+    unless user
+      user = User.new(
+          name: auth.info.name,
+          provider: auth.provider,
+          uid: auth.uid,
+          email: auth.info.email,
+          image_url: auth.info.image,
+          password: Devise.friendly_token[0, 20]
+      )
+      user.skip_confirmation!
+      user.save(validate: false)
+    end
+    user
+  end
+
   def self.create_unique_string
     SecureRandom.uuid
   end
