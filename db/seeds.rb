e5ccef5c2631c1bf1f790279f0b77c9619663c53
yuji05225818@gmail.com <yuji05225818@gmail.com>
2017-06-25 16:11:50 +0900

    タグのseedデータ投入とquestion.coffeeのインデント修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,3 +24,100 @@
 
   m = m + 1
 end
+
+  ActsAsTaggableOn::Tag.create!(
+               name: "javascript",
+               )
+  ActsAsTaggableOn::Tag.create!(
+               name: "swift",
+               )
+  ActsAsTaggableOn::Tag.create!(
+              name: "ios",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "android",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "ruby-on-rails",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "php",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "java",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "python",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "ruby",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "monaca",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "xcode",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "objective-c",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "html",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "jquery",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "mysql",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "linux",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "css",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "c++",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "unity3d",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "onsen-ui",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "windows",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "angularjs",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "html5",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "git",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "visual-studio",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "node.js",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "iphone",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "android-studio",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "cakephp",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "go",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "centos",
+              )
+  ActsAsTaggableOn::Tag.create!(
+              name: "sql",
+              )
